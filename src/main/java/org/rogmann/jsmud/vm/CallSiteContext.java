ffed9992a0fb56297caec8dcce8640aee2b246d3
rogmann <github@rogmann.org>
2021-08-30 23:18:49 +0200

    Execution of non-simulated methods in call-site-classes.

--- src/main/java/org/rogmann/jsmud/vm/CallSiteContext.java
+++ src/main/java/org/rogmann/jsmud/vm/CallSiteContext.java
@@ -1,15 +1,22 @@
 package org.rogmann.jsmud.vm;
 
 import java.lang.reflect.Executable;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
 
 import org.objectweb.asm.Handle;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
+import org.rogmann.jsmud.log.Logger;
+import org.rogmann.jsmud.log.LoggerFactory;
 
 /**
  * Context-information to execute JSMUD out of a call-site-instance.
  */
 public class CallSiteContext {
+	/** logger */
+	private static final Logger LOG = LoggerFactory.getLogger(CallSiteContext.class);
 
 	/** class-registry (VM) */
 	private final ClassRegistry registry;
@@ -45,25 +52,53 @@ public class CallSiteContext {
 			throw new JvmException(String.format("Can't load class (%s) to execute a call-site with method-handle (%s) in owner-class (%s)",
 					methodClassName, methodHandle, classOwner), e);
 		}
+
+		final Object objReturn;
 		final SimpleClassExecutor executor = registry.getClassExecutor(classMethod);
 		if (executor == null) {
-			throw new JvmException(String.format("No executor for (%s) to execute a call-site with method-handle (%s) in owner-class (%s)",
-					classMethod, methodHandle, classOwner));
+			// The method should be executed without simulation.
+			final Type methodType = Type.getMethodType(methodHandle.getDesc());
+			final Type[] types = methodType.getArgumentTypes();
+			final Type returnType = methodType.getReturnType();
+			final Method method = MethodFrame.findMethodInClass(methodHandle.getName(), types, returnType, classMethod);
+			if (method == null) {
+				throw new JvmException(String.format("Can't find method (%s) in class (%s)",
+						methodHandle.getName(), methodHandle.getDesc(), classMethod));
+			}
+			if (LOG.isDebugEnabled()) {
+				LOG.debug(String.format("executeMethod: non-simulated execution of %s%s in %s",
+						methodHandle.getName(), methodHandle.getDesc(), classMethod));
+			}
+			
+			method.setAccessible(true);
+			final Object obj;
+			final Object[] mArgs;
+			if (Modifier.isStatic(method.getModifiers())) {
+				obj = classMethod;
+				mArgs = args;
+			}
+			else {
+				obj = args[0];
+				mArgs = Arrays.copyOfRange(args, 1, args.length);
+			}
+			objReturn = method.invoke(obj, mArgs);
 		}
-		final int opcodeInvoke = lookupInvokeOpcode(classOwner, methodHandle);
-		final Type[] argTypes = Type.getArgumentTypes(methodHandle.getDesc());
-		final Class<?>[] aArgs = new Class<?>[argTypes.length];
-		for (int i = 0; i < argTypes.length; i++) {
-			final Type type = argTypes[i];
-			final Class<?> clazz = convertTypeToClass(type);
-			aArgs[i] = clazz;
+		else {
+			final int opcodeInvoke = lookupInvokeOpcode(classOwner, methodHandle);
+			final Type[] argTypes = Type.getArgumentTypes(methodHandle.getDesc());
+			final Class<?>[] aArgs = new Class<?>[argTypes.length];
+			for (int i = 0; i < argTypes.length; i++) {
+				final Type type = argTypes[i];
+				final Class<?> clazz = convertTypeToClass(type);
+				aArgs[i] = clazz;
+			}
+			final Executable method = classMethod.getDeclaredMethod(methodHandle.getName(), aArgs);
+			final OperandStack stack = new OperandStack(args.length);
+			for (int i = 0; i < args.length; i++) {
+				stack.push(args[i]);
+			}
+			objReturn = executor.executeMethod(opcodeInvoke, method, methodHandle.getDesc(), stack);
 		}
-		final Executable method = classMethod.getDeclaredMethod(methodHandle.getName(), aArgs);
-		final OperandStack stack = new OperandStack(args.length);
-		for (int i = 0; i < args.length; i++) {
-			stack.push(args[i]);
-		}
-		final Object objReturn = executor.executeMethod(opcodeInvoke, method, methodHandle.getDesc(), stack);
 		return objReturn;
 	}
 
