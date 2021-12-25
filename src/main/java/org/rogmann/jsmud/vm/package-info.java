83d5dfb71aa7d4e9a44e61788aa8a4abdd188481
rogmann <github@rogmann.org>
2021-12-25 20:35:06 +0100

    Javadoc (link VM-interface).

--- src/main/java/org/rogmann/jsmud/vm/package-info.java
+++ src/main/java/org/rogmann/jsmud/vm/package-info.java
@@ -1,7 +1,7 @@
 /**
  * This package contains classes to execute bytecode.
  * 
- * <p>The "JVM" is located in the VM implementing class {@link org.rogmann.jsmud.vm.ClassRegistry}.</p>
+ * <p>The "JVM" is located in the {@link org.rogmann.jsmud.vm.VM} implementing class {@link org.rogmann.jsmud.vm.ClassRegistry}.</p>
  * <p>The "bytecode execution" is located in the class {@link org.rogmann.jsmud.vm.MethodFrame}.</p>
  */
 package org.rogmann.jsmud.vm;
